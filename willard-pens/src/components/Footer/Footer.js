e1593cc8c8dbe18ab628347438030b17fb368885
Matthew Willard <MWillard85@gmail.com>
2020-03-27 11:12:19 -0600

    footer js

---
+++ willard-pens/src/components/Footer/Footer.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Footer.css";
+
+function Footer() {
+  return (
+    <footer className="Footer">
+    <div>
+        <p>Matthew Willard - 2019</p>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
