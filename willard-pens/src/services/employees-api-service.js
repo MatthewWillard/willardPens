078027431ec682a608364b95750514db416b8aba
Matthew Willard <MWillard85@gmail.com>
2020-10-15 18:29:39 -0600

    updating final

--- willard-pens/src/services/employees-api-service.js
+++ willard-pens/src/services/employees-api-service.js
@@ -1,13 +1,7 @@
 import config from "../config";
 import TokenService from "./token-service";
 
-const employeesApiService = {
-  getemployees() {
-    return fetch(`${config.API_ENDPOINT}/employees`, {
-      method: "GET",
-      headers: {
-        authorization: `bearer ${TokenService.getAuthToken()}`
-      }
+
 
 
 
